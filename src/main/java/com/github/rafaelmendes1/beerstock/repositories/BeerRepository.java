8672b6b42676a63df14858a9d86ec4c3e50e92b2
rafaelmendes1 <rarafafa3245@gmail.com>
2021-08-24 11:45:03 -0300

    Repository stereotype

--- src/main/java/com/github/rafaelmendes1/beerstock/repositories/BeerRepository.java
+++ src/main/java/com/github/rafaelmendes1/beerstock/repositories/BeerRepository.java
@@ -2,6 +2,12 @@ package com.github.rafaelmendes1.beerstock.repositories;
 
 import com.github.rafaelmendes1.beerstock.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
 }
